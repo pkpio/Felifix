9f6c24e686d72597f9df0c7185434b775465493b
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-03-26 19:52:40 +0530

    new-template-integration-started
    Only few sections done.

---
+++ js/script.js
@@ -0,0 +1,576 @@
+jQuery(document).ready(function(){
+
+	"use strict";
+	
+$(window).scroll(function(event) {
+	$(".flashing").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flash"); 
+		} 
+	});
+	$(".shaking").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated shake"); 
+		} 
+	});
+	$(".bouncing").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounce"); 
+		} 
+	});
+	$(".taada").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated tada"); 
+		} 
+	});
+	$(".swinging").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated swing"); 
+		} 
+	});		
+	$(".wobbling").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated wobble"); 
+		} 
+	});		
+	$(".pulse-beat").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated pulse"); 
+		} 
+	});
+	$(".fliping").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flip"); 
+		} 
+	});
+	$(".flipinx").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flipInX"); 
+		} 
+	});		
+	$(".flipoutx").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flipOutX"); 
+		} 
+	});		
+	$(".flipiny").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flipInY"); 
+		} 
+	});		
+	$(".flipouty").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated flipOutY"); 
+		} 
+	});		
+	$(".fadein").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeIn"); 
+		} 
+	});	
+	$(".fadeinup").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInUp"); 
+		} 
+	});
+	$(".fadeindown").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInDown"); 
+		} 
+	});
+	$(".fadeinleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInLeft"); 
+		} 
+	});
+	$(".fadeinright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInRight"); 
+		} 
+	});
+	$(".fadeinupbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInUpBig"); 
+		} 
+	});
+	$(".fadeindownbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInDownBig"); 
+		} 
+	});
+	$(".fadeinleftbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInLeftBig"); 
+		} 
+	});
+	$(".fadeinrightbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInRightBig"); 
+		} 
+	});
+	$(".fadeout").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOut"); 
+		} 
+	});	
+	$(".fadeoutup").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeInUp"); 
+		} 
+	});
+	$(".fadeoutdown").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutDown"); 
+		} 
+	});
+	$(".fadeoutleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutLeft"); 
+		} 
+	});
+	$(".fadeoutright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutRight"); 
+		} 
+	});
+	$(".fadeoutupbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutUpBig"); 
+		} 
+	});
+	$(".fadeoutdownbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutDownBig"); 
+		} 
+	});
+	$(".fadeoutleftbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutLeftBig"); 
+		} 
+	});
+	$(".fadeoutrightbig").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated fadeOutRightBig"); 
+		} 
+	});
+	$(".slideindown").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideInDown"); 
+		} 
+	});
+	$(".slideinleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideInLeft"); 
+		} 
+	});
+	$(".slideinright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideInRight"); 
+		} 
+	});	
+	$(".slideoutup").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideOutUp"); 
+		} 
+	});
+	$(".slideoutleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideOutLeft"); 
+		} 
+	});
+	$(".slideoutright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated slideOutRight"); 
+		} 
+	});	
+	$(".bouncein").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceIn"); 
+		} 
+	});		
+	$(".bounceinup").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceInUp"); 
+		} 
+	});
+	$(".bounceindown").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceInDown"); 
+		} 
+	});
+	$(".bounceinleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceInLeft"); 
+		} 
+	});
+	$(".bounceinright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceInRight"); 
+		} 
+	});
+	$(".bounceout").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceOut"); 
+		} 
+	});		
+	$(".bounceoutup").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceOutUp"); 
+		} 
+	});
+	$(".bounceoutdown").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceOutDown"); 
+		} 
+	});
+	$(".bounceoutleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceOutLeft"); 
+		} 
+	});
+	$(".bounceoutright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated bounceOutRight"); 
+		} 
+	});
+	$(".rotatein").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateIn"); 
+		} 
+	});
+	$(".rotateinupleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateInUpLeft"); 
+		} 
+	});
+	$(".rotateindownleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateInDownLeft"); 
+		} 
+	});
+	$(".rotateinupright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateInUpRight"); 
+		} 
+	});
+	$(".rotateindownright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateInDownRight"); 
+		} 
+	});
+	$(".rotateout").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateOut"); 
+		} 
+	});
+	$(".rotateoutupleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateOutUpLeft"); 
+		} 
+	});
+	$(".rotateoutdownleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateOutDownLeft"); 
+		} 
+	});
+	$(".rotateoutupright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateOutUpRight"); 
+		} 
+	});
+	$(".rotateoutupright").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateOutDownRight"); 
+		} 
+	});
+	$(".lightspeedin").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated lightSpeedIn"); 
+		} 
+	});	
+	$(".lightspeedout").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated lightSpeedOut"); 
+		} 
+	});	
+	$(".hinging").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated hinge"); 
+		} 
+	});	
+	$(".rollin").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rollIn"); 
+		} 
+	});	
+	$(".rollout").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rollOut"); 
+		} 
+	});	
+	
+	$(".rotateinupleft").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+		  el.addClass("animated rotateInUpLeft"); 
+		} 
+	});
+
+});
+	
+	
+	
+/*** ACCORDIONS ***/	
+$(function() {
+    $('#toggle .content').hide();
+    $('#toggle h3:first').addClass('active').next().slideDown(300);
+    $('#toggle h3').click(function() {
+        if($(this).next().is(':hidden')) {
+            $('#toggle h3').removeClass('active').next().slideUp(300);
+			$(this).toggleClass('active').next().slideDown(300);
+	 }
+    });
+});
+
+	
+/*** TEAM ***/	
+	var team = $('.team-carousel .team .member');
+	$(team).click(function(e){
+		$(this).parent().find('.member').removeClass('active');
+		$(this).addClass('active');
+	});	
+
+
+/* Check width on page load*/
+if ( $(window).width() < 980) {
+ $('nav').addClass('respsonsive-menu');
+}
+else {}
+
+$(window).resize(function() {
+/*If browser resized, check width again */
+if ($(window).width() < 980) {
+ $('nav').addClass('respsonsive-menu');
+}
+else {
+ $('nav').removeClass('respsonsive-menu');
+}	
+	
+});
+	$('nav.respsonsive-menu li').click(function(){
+		$('ul',this).slideToggle();
+	});	
+	
+	
+	
+
+	$('.open-menu').click(function(e){
+		$(this).toggleClass('opened');
+		$(this).parent().toggleClass('move');
+	});	
+
+
+	$('.fixed-top').click(function(){
+		$('nav').removeClass('stick-bottom');
+	});	
+	$('.fixed-bottom').click(function(){
+		$('nav').addClass('stick-bottom');
+	});	
+	
+
+/*** FIXED SIDEMENU APPEARS ON SCROLL DOWN ***/	
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+	if (scroll >= 50) {
+		$("nav").addClass("sticky animated bounceInDown");
+		$("nav").removeClass("bounceInUp");
+	}
+	else{
+		$("nav").removeClass("sticky animated bounceInDown");
+		$("nav").addClass("animated bounceInUp");
+	}
+});	
+$(window).scroll(function() {    
+	var scroll2 = $(window).scrollTop();
+	if (scroll2 >= 50) {
+		$("nav.hide-menu").addClass("show-menu  animated bounceInUp");
+	}
+	else{
+		$("nav.hide-menu").removeClass("show-menu  animated bounceInUp");
+	}
+});	
+
+	
+	
+/*** SET HEIGHT BASED ON VIEW PORT ***/				
+function thirty_pc() {
+    var height = $(window).height();
+    var thirtypc = (100 * height) / 100;
+    thirtypc = parseInt(thirtypc) + 'px';
+    $(".text-slider").css('height',thirtypc);
+}
+$(document).ready(function() {
+    thirty_pc();
+    $(window).bind('resize', thirty_pc);
+	
+});			
+			
+	
+	
+	
+/*** SMOOTH SCROLLING ***/	
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    }
+  });
+});
+
+
+var lastId,
+    topMenu = $("nav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});			
+	
+
+/*** CONTACT FORM VALIDATION ***/	
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+	if (scroll >= 50) {
+		$(".stick-header").addClass("sticky");
+	}
+	else{
+		$(".stick-header").removeClass("sticky");
+	}
+});
+	$('#contactform').submit(function(){
+		var action = $(this).attr('action');
+		$("#message").slideUp(750,function() {
+		$('#message').hide();
+ 		$('#submit')
+			.after('<img src="./images/ajax-loader.gif" class="loader" />')
+			.attr('disabled','disabled');
+
+		$.post(action, {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			phone: $('#phone').val(),
+			subject: $('#subject').val(),
+			comments: $('#comments').val(),
+			verify: $('#verify').val()
+		},
+			function(data){
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown('slow');
+				$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
+				$('#submit').removeAttr('disabled');
+				if(data.match('success') != null) $('#contactform').slideUp('slow');
+
+			}
+		);
+		});
+		return false;
+});
+		
+
+/*** SITE LOADING STYLE ***/	
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#preloader').delay(350).fadeOut(1000); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow':'visible'});
+})
+	
+});	
